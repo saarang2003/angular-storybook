055a4c8784cf17806063ad8e49d43b18dcb1a368
sarang <saranggp2018@gmail.com>
2025-05-01 18:02:12 +0530

    completed checkbox component story

---
+++ src/app/components/checkbox/checkbox.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ui-checkbox',
+  imports : [CommonModule],
+  standalone: true,
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.css'],
+})
+export class CheckboxComponent {
+  @Input() label: string = '';
+  @Input() checked: boolean = false;
+
+  // For multi-select: the key value and bound array of selections
+  @Input() value: string = '';
+  @Input() selectedValues: string[] = [];
+
+  toggleSingle() {
+    this.checked = !this.checked;
+  }
+
+  toggleMultiple() {
+    const index = this.selectedValues.indexOf(this.value);
+    if (index > -1) {
+      this.selectedValues.splice(index, 1);
+    } else {
+      this.selectedValues.push(this.value);
+    }
+  }
+}
---
+++ src/app/components/checkbox/checkbox.stories.ts
@@ -0,0 +1,46 @@
+
+
+import { Meta, StoryFn } from '@storybook/angular';
+import { CheckboxComponent } from './checkbox.component';
+import { FormsModule } from '@angular/forms';
+
+export default {
+  title: 'Components/Checkbox',
+  component: CheckboxComponent,
+  decorators: [
+    (story) => ({
+      template: `<div style="margin: 1rem;">${story().template}</div>`,
+      props: story().props,
+      moduleMetadata: {
+        imports: [FormsModule],
+      },
+    }),
+  ],
+  argTypes: {
+    label: { control: 'text' },
+    checked: { control: 'boolean' },
+  },
+} as Meta;
+
+const Template: StoryFn = (args) => ({
+  component: CheckboxComponent,
+  props: args,
+});
+
+export const SingleCheckbox = Template.bind({});
+SingleCheckbox.args = {
+  label: 'Click on the box ',
+  checked: false,
+};
+
+export const MultipleCheckboxes = () => ({
+  template: `
+    <ui-checkbox label="Apple" value="Apple" [selectedValues]="selectedValues"></ui-checkbox>
+    <ui-checkbox label="Banana" value="Banana" [selectedValues]="selectedValues"></ui-checkbox>
+    <ui-checkbox label="Mango" value="Mango" [selectedValues]="selectedValues"></ui-checkbox>
+    <p>Click on the above boxes ⬆️</p>
+  `,
+  props: {
+    selectedValues: [],
+  },
+});
