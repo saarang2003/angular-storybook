dab80dfbad8ec78137f8ff9dd5c19209431afed9
sarang <saranggp2018@gmail.com>
2025-05-13 13:40:09 +0530

    mobile responsive navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, HostListener } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports : [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
